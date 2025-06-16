08ea24c14ac0c835c111b2fb0d7973b97a6c20af
Arnold <Arnold>
2025-06-16 10:14:00 +0300

    regeistered the controllers

--- server/app.py
+++ server/app.py
@@ -14,4 +14,14 @@ def create_app():
     migrate.init_app(app, db)
     from server.models import pizza, restaurant, restaurant_pizza
 
+    from controllers.restaurant_controller import restaurant_bp
+    from controllers.pizza_controller import pizza_bp
+    from controllers.restaurant_pizza_controller import restaurant_pizza_bp
+
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(restaurant_pizza_bp)
+
     return app
+
+app=create_app()
